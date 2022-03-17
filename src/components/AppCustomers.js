9d9aa04ba6b3e05abfadcd277baf714fee565d0c
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-8.local>
2022-03-17 19:34:16 +0100

    zadatak 3

--- src/components/AppCustomers.js
+++ src/components/AppCustomers.js
@@ -2,32 +2,71 @@ import React, { useState } from 'react';
 
 function AppCustomers() {
 
+    const [newCustomer, setNewCustomer] = useState({
+        firstName: '',
+        lastName: '',
+      });
+
     const [customers, setCustomers] = useState([
-        {id: '1', name: 'Gohan'},
-        {id: '2', name: 'Goku'},
-        {id: '3', name: 'Bulma'},
-        {id: '4', name: 'Videl'},
-        {id: '5', name: 'Chi Chi'}
+        {firstName: 'Gohan', lastName:'Son'},
+        {firstName: 'Goku', lastName:'Son'},
+        {firstName: 'Bulma'},
+        {firstName: 'Videl', lastName:'Son'},
+        {firstName: 'Chi Chi', lastName:'Son'}
       ]);
+
+      const addCustomer = (e) => {
+        e.preventDefault();
+    
+        setCustomers([...customers, newCustomer])
+        setNewCustomer({
+          firstName: '',
+          lastName: ''
+        });
+      }
+
+      const handleFirstNameChange = (e) => {
+        setNewCustomer({
+          ...newCustomer,
+          firstName: e.target.value
+        })}
+
+        const handleLastNameChange = (e) => {
+            setNewCustomer({
+              ...newCustomer,
+              lastName: e.target.value
+            })}
+
+        
 
       const handleRemoveCustomer = (index) => {
         setCustomers([...customers.slice(0, index), ...customers.slice(index + 1)])
       }
 
     return (<div>
+        <div>
+      <h3>New Customer</h3>
+      <form className="form" onSubmit={addCustomer}>
+        <input required type="text" value={newCustomer.firstName}  placeholder="First name" onChange={handleFirstNameChange}/>
+        <br></br>
+        <input required type="text" value={newCustomer.lastName}  placeholder="Last name" onChange={handleLastNameChange}/>
+        
+        <button>Add</button>
+      </form>
+    </div>
     <h2>Customers:</h2>
     <table>
       <thead>
         <tr>
-            <th>Id</th>
-            <th>Name</th>
+            <th>First name</th>
+            <th>Last name</th>
         </tr>
         </thead>
         <tbody>
         {customers.map((customer, index) =>
            (<tr key={index}>
-            <td>{customer.id}</td>
-            <td>{customer.name}</td>
+            <td>{customer.firstName}</td>
+            <td>{customer.lastName}</td>
             <td><button onClick={() => handleRemoveCustomer(index)}>Remove</button></td>
           </tr>
         ))}
